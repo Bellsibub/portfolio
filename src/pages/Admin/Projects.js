0f9c3c0fd1a07d4fe5e22a543d41ce3ea3c97ac8
Bellsibub <isabella.billgren@gmail.com>
2023-01-26 10:40:46 +0100

    projects admin list actions and style

--- src/pages/Admin/Projects.js
+++ src/pages/Admin/Projects.js
@@ -1,9 +1,13 @@
 import { useDB } from 'hooks/useDB';
 import { useSnapshotDB } from 'hooks/useSnapshotDB';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { regular } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+import list from 'styles/List.module.css';
 
 const Projects = () => {
-  const { addDocument } = useDB('projects');
+  const { addDocument, updateDocument, deleteDocument } = useDB('projects');
   const { documents } = useSnapshotDB('projects', false);
   const navigate = useNavigate();
 
@@ -14,12 +18,25 @@ const Projects = () => {
 
   return (
     <div>
-      <button onClick={handleAddNewDoc}>create new</button>
       {documents && (
-        <ul>
+        <ul className={list.container}>
+          <button onClick={handleAddNewDoc}>create new</button>
           {documents.map((doc) => (
-            <li key={doc.id} onClick={() => navigate(doc.id)}>
-              {doc.title}
+            <li className={list.item} key={doc.id}>
+              <p onClick={() => navigate(doc.id)}>{doc.title}</p>
+              <div className={list.actions}>
+                <FontAwesomeIcon
+                  onClick={() =>
+                    updateDocument(doc.id, { featured: !doc.featured })
+                  }
+                  className={doc.featured ? list.active : ''}
+                  icon={regular('star')}
+                />
+                <FontAwesomeIcon
+                  onClick={() => deleteDocument(doc.id)}
+                  icon={regular('trash-can')}
+                />
+              </div>
             </li>
           ))}
         </ul>
