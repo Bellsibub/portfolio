39764fac68c56e9b0154157d2fddcb1bfd5d678c
Bellsibub <isabella.billgren@gmail.com>
2023-03-06 10:07:48 +0100

    /contact

--- src/pages/Main/Contact.js
+++ src/pages/Main/Contact.js
@@ -1,8 +1,22 @@
+import { ResumeButton, SocialLinks } from 'components';
+import layout from './Layouts/Contact.module.css';
 
 const Contact = () => {
   return (
     <>
-      <div>Contact</div>
+      <div className={layout.wrapper}>
+        <ResumeButton />
+        <div className={layout.section}>
+          <h4>email</h4>
+          <a className={layout.link} href="mailto:isabella.billgren@gmail.com">
+            isabella.billgren@gmail.com
+          </a>
+        </div>
+        <div className={layout.section}>
+          <h4>socials</h4>
+          <SocialLinks />
+        </div>
+      </div>
     </>
   );
 };
