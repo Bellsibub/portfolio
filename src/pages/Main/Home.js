e5cb83f50ea3b060274c5880230c75145bce5d91
Bellsibub <isabella.billgren@gmail.com>
2023-03-06 09:59:30 +0100

    /home

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,10 +1,23 @@
 import { ResumeButton, SocialLinks } from 'components';
 
+import layout from './Layouts/Home.module.css';
+
 const Home = () => {
   return (
     <>
-      <SocialLinks />
-      <ResumeButton />
+      <div className={layout.wrapper}>
+        <h1>Isabella Billgren</h1>
+        <h4>frontend developer</h4>
+        <p>
+          I am a passionate frontend developer whos quest for improving the web
+          has only just begun. Gaining knowledge and experience throughout my +5
+          years in the IT industry as a QA Engineer has led me to take on this
+          new challenge.
+        </p>
+        <p>The journey has only just begun…</p>
+        <ResumeButton />
+        <SocialLinks />
+      </div>
     </>
   );
 };
