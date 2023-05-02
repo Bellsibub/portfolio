e6e4d0387393f4f66e1944ead7867736677254af
Bellsibub <isabella.billgren@gmail.com>
2023-05-02 10:27:43 +0200

    quest mobile layout

--- src/pages/Main/Quests.js
+++ src/pages/Main/Quests.js
@@ -5,8 +5,11 @@ import { useSnapshotDB } from 'hooks/useSnapshotDB';
 import { FilterGroup, ListHeader } from 'components';
 // layout
 import layout from './Layouts/Quests.module.css';
+import { useViewport } from 'hooks/useViewport';
 
 const Quests = () => {
+  const { width } = useViewport();
+  const breakpoint = 725;
   let { documents } = useSnapshotDB('projects', true);
   let { documents: featuredDocs } = useSnapshotDB('projects', false, [
     'featured',
@@ -15,32 +18,50 @@ const Quests = () => {
   ]);
   let groupedDocs = _.groupBy(documents, 'primarySkill');
 
-  if (featuredDocs)
-    return (
+  if (documents)
+    return width > breakpoint ? (
       <div className={layout.wrapper}>
         <FilterGroup className={layout.filters} />
-        {groupedDocs ? (
-          <div className={layout.list}>
+        <div className={layout.list}>
+          <ListHeader
+            featured
+            list={featuredDocs}
+            title="featured"
+            projectCount
+            accordian
+          />
+          {Object.entries(groupedDocs).map(([skillname, projects]) => (
             <ListHeader
-              featured
-              list={featuredDocs}
-              title="featured"
+              key={skillname}
+              list={projects}
+              title={skillname}
               projectCount
               accordian
             />
-            {Object.entries(groupedDocs).map(([skillname, projects]) => (
-              <ListHeader
-                key={skillname}
-                list={projects}
-                title={skillname}
-                projectCount
-                accordian
-              />
-            ))}
-          </div>
-        ) : (
-          <div>no documents found</div>
-        )}
+          ))}
+        </div>
+      </div>
+    ) : (
+      <div className={layout.wrapper}>
+        <div className={layout.list}>
+          <ListHeader
+            featured
+            list={featuredDocs}
+            title="featured"
+            projectCount
+            accordian
+          />
+        <FilterGroup className={layout.filters} />
+          {Object.entries(groupedDocs).map(([skillname, projects]) => (
+            <ListHeader
+              key={skillname}
+              list={projects}
+              title={skillname}
+              projectCount
+              accordian
+            />
+          ))}
+        </div>
       </div>
     );
 };
